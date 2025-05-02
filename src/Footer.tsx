a2554385535132d23a8f5ef944dbaf5cc4fcbf19
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-03 01:30:12 +0300

    Initial project setup with Vite and file structure

---
+++ src/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://NoOne.com">
+            NoOne
+          </a>
+        </p>
+        <p>
+          <span>All Rights Reserved.</span>
+        </p>
+      </small>
+    </footer>
+  );
+}
