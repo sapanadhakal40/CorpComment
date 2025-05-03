215f8728fbca1879ef46d56ccc47f7538dfb4048
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-03 20:30:51 +0300

    Add conditional rendering to show NEW for new feedback

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -21,7 +21,7 @@ export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
           <p>{feedbackItem.text}</p>
         </div>
 
-        <p>{feedbackItem.daysAgo}d</p>
+        <p>{feedbackItem.daysAgo === 0 ? "NEW" : `${feedbackItem.daysAgo}d`}</p>
       </li>
     </div>
   );
