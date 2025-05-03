bc28bd7af7afd4490e17fec250c1f67bc801eea4
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-04 01:33:44 +0300

    Create feedback items context provider

---
+++ src/context/FeedbackItemsContextProvider.tsx
@@ -0,0 +1,89 @@
+import { createContext, useEffect, useMemo, useState } from "react";
+import { TFeedbackItem } from "../lib/type";
+
+type TFeedbackItemsContext = {
+  isLoading: boolean;
+  feedbackItems: TFeedbackItem[];
+  errorMessage: string;
+  companyList: string[];
+  handleAddToList: (text: string) => void;
+};
+const feedbackItemsContext = createContext<TFeedbackItemsContext | null>(null);
+
+export default function FeedbackItemsContextProvider() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const companyList = useMemo(
+    () =>
+      feedbackItems
+        .map((item) => item.company)
+        .filter((company, index, array) => {
+          return array.indexOf(company) === index;
+        }),
+    [feedbackItems]
+  );
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+    setFeedbackItems([...feedbackItems, newItem]);
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: "application/json",
+          "content-type": "application/json",
+        },
+      }
+    );
+  };
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Something went wrong. Please try again later");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
+  return (
+    <feedbackItemsContext.Provider
+      value={{
+        feedbackItems,
+        isLoading,
+        errorMessage,
+        companyList,
+        handleAddToList,
+      }}
+    ></feedbackItemsContext.Provider>
+  );
+}
