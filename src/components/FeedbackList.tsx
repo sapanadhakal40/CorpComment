7493029e1fea44bda29bc16f9a5565390b7ada23
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-03 15:41:41 +0300

    Create feedback items list by mapping an array

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,28 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import FeedbackItem from "./FeedbackItem";
+
+const feedbackItems = [
+  {
+    upvoteCount: 593,
+    badgeLetter: "B",
+    companyName: "Byte",
+    text: "test test",
+    daysAgo: 4,
+  },
+  {
+    upvoteCount: 493,
+    badgeLetter: "A",
+    companyName: "Amazon",
+    text: "This is a test",
+    daysAgo: 2,
+  },
+];
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>NoOne</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
-            placeat ipsam ut impedit, iusto accusantium?
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {feedbackItems.map((feedbackItem) => {
+        return <FeedbackItem feedbackItem={feedbackItem} />;
+      })}
     </ol>
   );
 }
