0ffcce6944a55f49103fc7dd934fc30045218a4f
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-03 16:57:24 +0300

    Add basic error message display functionality

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,39 @@
 import FeedbackItem from "./FeedbackItem";
 import { useEffect, useState } from "react";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/asdafasadf"
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
         return response.json();
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        //network error, not 200 response, json parsing error
+        setErrorMessage("Something went wrong.");
         setIsLoading(false);
       });
   }, []);
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
       {feedbackItems.map((feedbackItem) => {
         return (
           <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
