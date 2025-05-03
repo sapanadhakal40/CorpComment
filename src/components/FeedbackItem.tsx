84130ce229c7c34293e1b29a4e2744f0727974f2
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-03 18:14:16 +0300

    Fix types

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,14 +1,8 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { TFeedbackItem } from "../lib/type";
 
-type FeedbackItem = {
-  upvoteCount: number;
-  badgeLetter: string;
-  companyName: string;
-  text: string;
-  daysAgo: number;
-};
 type FeedbackItemProps = {
-  feedbackItem: FeedbackItem;
+  feedbackItem: TFeedbackItem;
 };
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   return (
