80b9a709ae4fd64721afdd930b3d67225331a087
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-04 03:01:46 +0300

    update feedback list item with filtered feedback items and use custom hooks

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,17 +1,18 @@
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "../Spinner";
 import ErrorMessage from "../ErrorMessage";
-import { useFeedbackItemsContext } from "../../context/FeedbackItemsContextProvider";
+import { useFeedbackItemsContext } from "../../lib/hooks";
 
 export default function FeedbackList() {
-  const { isLoading, errorMessage, feedbackItems } = useFeedbackItemsContext();
+  const { isLoading, errorMessage, filteredFeedbackItems } =
+    useFeedbackItemsContext();
 
   return (
     <ol className="feedback-list">
       {isLoading && <Spinner />}
       {errorMessage && <ErrorMessage message={errorMessage} />}
 
-      {feedbackItems.map((feedbackItem) => {
+      {filteredFeedbackItems.map((feedbackItem) => {
         return (
           <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
         );
